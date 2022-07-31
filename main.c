051fd3e7d89e1e31a8862241cf05fc08b4629cef
e-man <tenhitcombo@gmail.com>
2022-07-31 14:35:29 -0400

    main w/ threads that return

--- main.c
+++ main.c
@@ -6,10 +6,14 @@ void* hello_return(void* args);
 int main()
 {
     char* str;
-    pthread_t thread;
+    pthread_t thread; // thread identifier
 
     //create a new thread tha turns hello_return without arguments
     pthread_create(&thread, NULL, hello_return, NULL);
+
+    // wait until the thread completes, assign return value to str
+    pthread_join(thread, (void**) & str);
+    printf("%s", str);
 
     return 0;
 }
